c3fdbe2c0506c10328b221a0524510a49156df1d
Joey Beninghove <me@joey.io>
2017-02-13 10:56:42 -0500

    Symlinks helper script

---
+++ symlinks.sh
@@ -0,0 +1,35 @@
+rm ~/.agignore
+rm ~/.aliases
+rm ~/.bash_profile
+rm ~/.bashrc
+rm ~/.gemrc
+rm ~/.git-completion
+rm ~/.git-prompt
+rm ~/.gitconfig
+rm ~/.gitignore
+rm ~/.irbrc
+rm ~/.prompt
+rm ~/.rspec
+rm ~/.tmux.conf
+rm ~/.vim
+rm ~/.vimrc
+rm ~/.vimrc.bundles
+rm -rf ~/-/bin
+
+ln -s ~/-/dotfiles/agignore ~/.agignore
+ln -s ~/-/dotfiles/aliases ~/.aliases
+ln -s ~/-/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/-/dotfiles/bashrc ~/.bashrc
+ln -s ~/-/dotfiles/gemrc ~/.gemrc
+ln -s ~/-/dotfiles/git-completion ~/.git-completion
+ln -s ~/-/dotfiles/git-prompt ~/.git-prompt
+ln -s ~/-/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/-/dotfiles/gitignore ~/.gitignore
+ln -s ~/-/dotfiles/irbrc ~/.irbrc
+ln -s ~/-/dotfiles/prompt ~/.prompt
+ln -s ~/-/dotfiles/rspec ~/.rspec
+ln -s ~/-/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/-/dotfiles/vim ~/.vim
+ln -s ~/-/dotfiles/vimrc ~/.vimrc
+ln -s ~/-/dotfiles/vimrc.bundles ~/.vimrc.bundles
+ln -s ~/-/dotfiles/bin ~/-/bin
